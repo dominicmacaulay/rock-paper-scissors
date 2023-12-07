18b1247a9a35c50656db7eb35b5e88ac500bb951
dominicmacaulay <dominic.62502@gmail.com>
2023-12-07 15:21:02 -0500

    Added working ui elements to the game
    Before now, this game was purely viewable through the console. With the
    addition of ui, it is now playable using buttons. A player vs. console
    score box is updated. And, once a player has reached 5 points, a text
    is displayed which indicates so.

--- program.js
+++ program.js
@@ -1,23 +1,81 @@
+const btnR = document.querySelector('#rock');
+const btnP = document.querySelector('#paper');
+const btnS = document.querySelector('#scissors');
+
+const textResults = document.querySelector('#results');
+const textWinner = document.querySelector('#winner');
+const textScore = document.querySelector('#score');
+
 let computerChoice;
-let playerInput;
 let computerScore = 0;
 let playerScore = 0;
-game();
 
-function game(){
-    while(computerScore < 5 && playerScore < 5){ 
-        playerInput = prompt("Your move...");
-        computerChoice = getComputerChoice();
-        console.log(computerChoice);
-        playRound(playerInput, computerChoice);
-        console.log(playerScore, computerScore);
-    }
+// whenever these buttons are clicked, they call the play round function
+// with whatever choice the player made as the value.
+btnR.addEventListener('click', () => {
+    playRound('rock');
+});
+btnP.addEventListener('click', () => {
+    playRound('paper');
+});
+btnS.addEventListener('click', () => {
+    playRound('scissors');
+});
+
+// display the starting score
+textScore.textContent = "Your Score: " + playerScore + " -- Opponent Score: " + computerScore;
+
+// function called to get the computer's choice and compare it with the 
+// player's choice.
+function playRound(playerChoice){
+    computerChoice = getComputerChoice();
+
+    // compare player guesses
+    if (computerChoice == "rock"){
+        if (playerChoice == "rock"){
+            textResults.textContent = "It's a tie";
+        }else if (playerChoice == "paper"){
+            playerScore ++;
+            textResults.textContent = "You won!";
+        }
+        else if (playerChoice == "scissors"){
+            computerScore ++;
+            textResults.textContent = "You Lost!";
+        }
+    }else if(computerChoice == "paper"){
+        if (playerChoice == "rock"){
+            computerScore ++;
+            textResults.textContent = "You Lost!";
+        }else if (playerChoice == "paper"){
+            textResults.textContent = "It's a tie";
+        }
+        else if (playerChoice == "scissors"){
+            playerScore ++;
+            textResults.textContent = "You won!";
+        }
+    } else if (computerChoice == "scissors"){
+        if (playerChoice == "rock"){
+            playerScore ++;
+            textResults.textContent = "You won!";
+        }else if (playerChoice == "paper"){
+            computerScore ++;
+            textResults.textContent = "You Lost!";
+        }
+        else if (playerChoice == "scissors"){
+            textResults.textContent = "It's a tie";
+        }
+    } else {console.log("something is wrong 2");}
+
+    // display score
+    textScore.textContent = "Your Score: " + playerScore + " -- Opponent Score: " + computerScore;
+
+    // check for win condiditon from either player
     if (computerScore == 5){
         console.log("you lose")
-        return "Loss";
+        textWinner.textContent = "Your Opponent Won The Game...";
     } else if (playerScore == 5){
         console.log("you win");
-        return "Win";
+        textWinner.textContent = "You Have Won The Game!";
     }
 }
 
@@ -30,44 +88,6 @@ function getComputerChoice(){
         case 1: return "rock";
         case 2: return "paper";
         case 3: return "scissors";
-        default: console.log("Something got fucked");
+        default: console.log("Something is wrong 1");
     }
-}
-
-function playRound(playerInput, computerChoice){
-    let playerChoice = playerInput.toLowerCase();
-    if (computerChoice == "rock"){
-        if (playerChoice == "rock"){
-            return "tie";
-        }else if (playerChoice == "paper"){
-            playerScore ++;
-            return "win";
-        }
-        else if (playerChoice == "scissors"){
-            computerScore ++;
-            return "lose";
-        }
-    }else if(computerChoice == "paper"){
-        if (playerChoice == "rock"){
-            computerScore ++;
-            return "lose";
-        }else if (playerChoice == "paper"){
-            return "tie";
-        }
-        else if (playerChoice == "scissors"){
-            playerScore ++;
-            return "win";
-        }
-    } else if (computerChoice == "scissors"){
-        if (playerChoice == "rock"){
-            playerScore ++;
-            return "win";
-        }else if (playerChoice == "paper"){
-            computerScore ++;
-            return "lose";
-        }
-        else if (playerChoice == "scissors"){
-            return "tie";
-        }
-    } else {console.log("something got fucked 2");}
 }
